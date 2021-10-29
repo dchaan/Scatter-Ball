c55f503bbb350066bf17577d1672e55630fdd36e
David Chan <dchaan1@gmail.com>
2021-10-29 11:58:22 -0700

    Troubleshooting setup files

--- src/index.js
+++ src/index.js
@@ -1,24 +1,27 @@
 /* I'd recommend adding a DOMContentLoaded event listener, 
 and in that callback doing some basic DOM manipulation. */
+document.addEventListener("DOMContentLoaded", () => {
+  console.log('Hello World')
+})
 
 // src/index.js
 
-const demoUrl = 'https://www.metaweather.com/api/location/search/?query=san';
-const corsRequest = (url = demoUrl) => {
-  fetch(`/cors?url=${encodeURIComponent(url)}`)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-    })
-}
+// const demoUrl = 'https://www.metaweather.com/api/location/search/?query=san';
+// const corsRequest = (url = demoUrl) => {
+//   fetch(`/cors?url=${encodeURIComponent(url)}`)
+//     .then(res => res.json())
+//     .then(data => {
+//       console.log(data);
+//     })
+// }
 
-const apiRequest = (query = 'curry') => {
-  fetch(`/api?searchTerm=${encodeURIComponent(query)}`)
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-    })
-}
+// const apiRequest = (query = 'curry') => {
+//   fetch(`/api?searchTerm=${encodeURIComponent(query)}`)
+//     .then(res => res.json())
+//     .then(data => {
+//       console.log(data);
+//     })
+// }
 
 /* 
 Before Refactor:
